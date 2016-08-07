051d74b478b04cee4c407b234c2550105b8d2c4a
sefp-test <mail-for-rails-tutorial-sefp@mail.ru>
2016-08-08 01:04:49 +0300

    Shorten routes, include root

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,17 +7,17 @@ describe 'Static pages' do
 	describe 'Home page' do
 		
 		it "should have the content 'Sample app'" do
-			visit '/static_pages/home'
+			visit root_path
 			expect(page).to have_content('Sample App')
 		end
 
 		it "shoult have base title" do
-			visit '/static_pages/home'
+			visit root_path
 			expect(page).to have_title("#{base_title}")
 		end
 
 		it "should not have custom title" do
-			visit '/static_pages/home'
+			visit root_path
 			expect(page).not_to have_title("| Home")
 		end
 	end
@@ -25,12 +25,12 @@ describe 'Static pages' do
 	describe 'Help page' do
 		
 		it "should have the content 'Help'" do
-			visit '/static_pages/help'
+			visit help_path
 			expect(page).to have_content('Help')			
 		end
 
 		it "shoult have title with 'Help'" do
-			visit '/static_pages/help'
+			visit help_path
 			expect(page).to have_title("#{base_title} | Help")
 		end
 	end
@@ -38,12 +38,12 @@ describe 'Static pages' do
 	describe 'About page' do
 		
 		it "should have the content 'About Us'" do
-			visit '/static_pages/about'
+			visit about_path
 			expect(page).to have_content('About Us')
 		end
 
 		it "shoult have title with 'About'" do
-			visit '/static_pages/about'
+			visit about_path
 			expect(page).to have_title("#{base_title} | About")
 		end
 	end
@@ -51,12 +51,12 @@ describe 'Static pages' do
 	describe 'Contact page' do
 		
 		it "it should have the content ''" do
-			visit '/static_pages/contact'
+			visit contact_path
 			expect(page).to have_content('Contact')
 		end
 
 		it "should have title with 'Content'" do
-			visit '/static_pages/contact'
+			visit contact_path
 			expect(page).to have_title("#{base_title} | Contact")
 		end
 	end
