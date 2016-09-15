40addb2016dc0134b90d47a0fd44aac412a79e36
sefp-test <mail-for-rails-tutorial-sefp@mail.ru>
2016-09-15 19:57:02 +0300

    Add form for user registration.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -9,6 +9,29 @@ describe 'User pages' do
 
 		it { should have_content('Sign up') }
 		it { should have_title(full_title('Sign up')) }
+
+		describe 'send user data' do
+			let(:submit) { "Create my account" }
+
+			describe 'with invalid information' do
+				it "should not create a user" do
+					expect { click_button submit }.not_to change(User, :count)
+				end
+			end
+
+			describe 'with correct information' do
+				before do
+					fill_in "Name", 				with: "Example User"
+					fill_in "Email", 				with: "user@example.com"
+					fill_in "Password", 		with: 'foobar'
+					fill_in "Confirmation", with: 'foobar'
+				end
+
+				it "should create a user" do
+					expect { click_button submit }.to change(User, :count).by(1)
+				end
+			end
+		end
 	end
 
 	describe 'profile page' do
